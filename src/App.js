3501b957dee364725d0412ff18bfe517cc3b66f1
Jay2009 <37827213+Jay2009@users.noreply.github.com>
2021-08-12 00:41:00 +0900

    Chapter6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import ValidationSaSmple from './ScrollBox.js';
 import './App.css';
+import ScrollBox from './ScrollBox.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div>
+      <ScrollBox ref={(ref) => this.scrollBox=ref} />
+      <button onClick={() => this.scrollBox.scrollToBottom()}>
+        go to the botton
+        </button>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
