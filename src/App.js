15fbca4d67950aa6f269f9c801db023fe7a19626
Jay2009 <37827213+Jay2009@users.noreply.github.com>
2021-08-12 02:27:17 +0900

    Chapter 6 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-//import ValidationSaSmple from './ScrollBox.js
 import './App.css';
-import ScrollBox from './ScrollBox.js';
+import InterationSample from './InterationSample';
 
 class App extends Component {
   render() {
     return(
-      <div>
-      <ScrollBox ref={(ref) => this.scrollBox=ref} />
-      <button onClick={() => this.scrollBox.scrollToBottom()}>
-        go to the botton
-        </button>
-        
-      </div>
+      <InterationSample />
     );
   }
 }
