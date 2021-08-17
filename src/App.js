6e1b8ed8040ff186a6488fbd0dee5d3e3598c568
Jay2009 <37827213+Jay2009@users.noreply.github.com>
2021-08-18 03:21:08 +0900

    done with handling array and useEffect(Mount)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,87 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
-
+import CreateUser from './CreateUser';
 
 
 function App() {
-  const users = [
+
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  
+  const [users, setUsers] = useState([
+    
     {
         id: 1,
         username: 'jay',
-        email: 'jay@gmail.com'
+        email: 'jay@gmail.com',
+        active: true
     },
     {
         id: 2,
         username: 'tester',
-        email: 'tester@example.com'
+        email: 'tester@example.com',
+        active: false
     },
     {
         id: 3,
         username: 'liz',
-        email: 'liz@example.com'
+        email: 'liz@example.com',
+        active: false
     }
-];
+  ]);
+  const nextId = useRef(4);
 
-const nextId = useRef(4);
-const onCreate = () => {
+  const onCreate = () => {
+  const user = {
+    id: nextId.current,
+    username,
+    email
+  };
 
+  setUsers(users.concat(user));
 
+  setInputs({
+    username: '',
+    email: ''
+  });
   nextId.current += 1;
 };
 
+const onRemove = id => {
+
+  setUsers(users.filter(user => user.id !== id));
+};
+
+const onToggle = id => {
+  setUsers(
+    users.map(user =>
+      user.id === id ? {...user, active: !user.active} : user
+      )
+  );
+};
 
   return (
-    <UserList users={users} />
+    <>
+    <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
   );
 }
 
